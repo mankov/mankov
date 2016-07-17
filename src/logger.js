50c96dabda5d2776b1dd4359d2c53447b7657a31
Miro Nieminen <miro.nieminen@futurice.com>
2016-07-17 18:24:15 +0300

    Prefix all logging labels with "mankov"

--- src/logger.js
+++ src/logger.js
@@ -5,12 +5,13 @@ const _       = require('lodash');
 const COLORIZE = process.env.NODE_ENV === 'development';
 
 function createLogger(filePath) {
-  var fileName = path.basename(filePath);
+  const fileName = path.basename(filePath);
+  const label = `mankov${fileName ? '-' + fileName : ''}`;
 
   const logger = new winston.Logger({
     transports: [new winston.transports.Console({
       colorize: COLORIZE,
-      label: fileName,
+      label,
       timestamp: true
     })]
   });
@@ -20,7 +21,7 @@ function createLogger(filePath) {
 }
 
 function _setLevelForTransports(logger, level) {
-  _.each(logger.transports, function(transport) {
+  _.each(logger.transports, transport => {
     transport.level = level;
   });
 }
