91b8ce4633dc1d8b46dc4b3b85e641ae9354573e
Miro Nieminen <miro.nieminen@futurice.com>
2016-07-22 01:30:31 +0300

    [WIP] Add stub for testing Telegram-platform module

---
+++ test/platform-telegram_spec.js
@@ -0,0 +1,27 @@
+const common = require('./common');
+const expect = require('chai').expect;
+
+const testData = require('./data/telegram-messages');
+
+const Platform = require('../src/platforms/telegram');
+
+describe('Telegram Platform', () => {
+  let platform = null;
+
+  before(() => {
+    platform = new Platform({
+      name: 'test',
+      client: {
+        token: 'abc-1337'
+      }
+    });
+  });
+
+
+  it('Parses raw Telegram message', () => {
+    const parsedEvent = platform.parseMessage(testData.rawIltaaMessage.message);
+
+    console.log('parsed event', parsedEvent);
+    // TODO actually check the result
+  });
+});
