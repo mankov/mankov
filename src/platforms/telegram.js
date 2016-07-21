722caa977a120aee51fecdd3450a0610cf89f012
Antti Kivimäki <majoriz11@gmail.com>
2016-07-21 21:58:36 +0300

    Parse Telegram messages

--- src/platforms/telegram.js
+++ src/platforms/telegram.js
@@ -1,4 +1,6 @@
-const tgClient = require('node-telegram-bot-api');
+const tgClient  = require('node-telegram-bot-api');
+const _         = require('lodash');
+const log       = require('../logger')(__filename);
 
 const BasePlatform = require('./base');
 
@@ -15,9 +17,47 @@ module.exports = class TelegramPlatform extends BasePlatform {
     });
   }
 
-  parseMessage(msg) {
+  parseMessage(raw) {
     let event = { type: 'telegram' };
-    // TODO!
+    event.eventId = raw.update_id;
+    event.meta = {};
+
+    if (raw.message || raw.edited_message) {
+      let msg = (raw.message) ? raw.message : raw.edited_message;
+      event.text = msg.text;
+      event.userId = msg.from.id;
+
+      // Parse metadata
+      event.meta.replyToMessage = raw.reply_to_message;
+      event.meta.userName = raw.from.username;
+      event.meta.userFirstName = raw.from.first_name;
+      event.meta.userLastName = raw.from.last_name;
+      event.meta.userCallName = _.isUndefined(event.userName) ?
+          event.meta.userFirstName :
+          `@${event.userName}`;
+
+      event.meta.isFromGroup = !_.isUndefined(raw.chat.title);
+      event.meta.chatGroupId = event.isFromGroup ? raw.chat.id : null;
+      event.meta.chatGroupTitle = event.isFromGroup ? raw.chat.title : null;
+
+      event.meta.editDate = (raw.edit_date) ? raw.edit_date : null;
+      event.meta.entities = (raw.entities) ? raw.entities : null;
+
+      event.meta.targetId = (event.isFromGroup) ? event.chatGroupId : event.userId;
+
+    } else if (raw.inline_query) {
+      // TODO: Parse inline query
+
+    } else if (raw.chosen_inline_query) {
+      // TODO: Parse ...
+
+    } else if (raw.callback_query) {
+      // TODO: Parse ...
+
+    } else {
+      log.error('Invalid Telegram message', raw);
+    }
+
     return event;
   }
 
